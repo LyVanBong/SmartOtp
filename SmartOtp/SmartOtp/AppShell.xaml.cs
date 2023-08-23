9ecbda4c604d145b7bd2708702c3d643faccb8ec
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2023-08-23 15:02:47 +0700

    Cập nhật lại project

---
+++ SmartOtp/SmartOtp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SmartOtp.ViewModels;
+using SmartOtp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SmartOtp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
